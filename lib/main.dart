ee87b8f9bba57660b395fb2659195cc66801df8e
alielrayes <61753691+alielrayes@users.noreply.github.com>
2022-03-06 23:15:33 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:todo_app_lesson10_1/widgets/todo-card.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TodoApp(),
+    );
+  }
+}
+
+class TodoApp extends StatefulWidget {
+  const TodoApp({Key? key}) : super(key: key);
+
+  @override
+  State<TodoApp> createState() => _TodoAppState();
+}
+
+class _TodoAppState extends State<TodoApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(58, 66, 86, 0.7),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color.fromRGBO(58, 66, 86, 1),
+        title: Text(
+          "TO DO APP",
+          style: TextStyle(
+              fontSize: 33, color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Todecard(),
+            Todecard(),
+            Todecard(),
+            Todecard(),
+            Todecard(),
+
+          
+          
+          ],
+        ),
+      ),
+    );
+  }
+}
